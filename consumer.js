afcf1bae1a0f956a7e07c08083462d6428088012
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-06 16:13:55 -0300

    fix

--- consumer.js
+++ consumer.js
@@ -1,23 +1,24 @@
-const { Kafka } = require('kafkajs')
+const { Kafka } = require('kafkajs');
 
 const kafka = new Kafka({
-  clientId: 'my-app',
-  brokers: ['localhost:9092'],
-})
+  clientId: 'my-app', 
+  brokers: ['localhost:9092'], 
+});
 
-const consumer = kafka.consumer({ groupId: 'test-group' })
+const consumer = kafka.consumer({ groupId: 'test-group' });
 
-const runConsumer = async () => {
-  await consumer.connect()
-  await consumer.subscribe({ topic: 'test-topic', fromBeginning: true })
+module.exports = {
+  consumer,
+  runConsumer: async (topic, callback) => {
+    await consumer.connect();
 
-  await consumer.run({
-    eachMessage: async ({ topic, partition, message }) => {
-      console.log({
-        value: message.value.toString(),
-      })
-    },
-  })
-}
+    await consumer.subscribe({ topic, fromBeginning: true });
 
-module.exports = { runConsumer, consumer }
+    await consumer.run({
+      eachMessage: async ({ message }) => {
+        const payload = JSON.parse(message.value.toString());
+        await callback(payload);
+      },
+    });
+  },
+};
