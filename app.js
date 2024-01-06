2e21af4c4cc0025ba4d09adc83aec08333089f4f
Riosberg Dutra <126841979+riosbergdutra@users.noreply.github.com>
2024-01-06 16:15:16 -0300

    colocando rota,database e port

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { producer } = require('./producer');
+const { consumer } = require('./consumer');
+const ConnectToDatabase = require("./src/database/database");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Servidor Express Kafka');
-});
+ConnectToDatabase();
+const port = 3000;
 
-const { runProducer } = require('./producer');
-const { runConsumer } = require('./consumer');
+app.use(bodyParser.json());
+app.use('/user', require('./src/router/userrouter'));
+app.use('/product', require('./src/router/productrouter')); 
+const runProducer = async () => {
+  await producer.connect();
+};
 
-const startServer = async () => {
-  await runProducer(); 
+const runConsumer = async () => {
+  await consumer.connect();
+};
+
+const startApp = async () => {
+  await runProducer();
   await runConsumer();
 
-  const server = app.listen(3000, () => {
-    console.log('Servidor Express rodando na porta 3000');
+  app.listen(port, () => {
+    console.log(`Servidor rodando na http://localhost:${port}`);
   });
 };
 
-startServer().catch(console.error);
+startApp().catch(console.error);
